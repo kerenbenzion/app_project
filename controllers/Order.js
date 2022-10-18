6a773260ece142f9e2523b7fe35ddc0b2733b726
barchen <barchen7200@gmail.com>
2022-10-18 19:09:29 +0300

    Add payment page

--- controllers/Order.js
+++ controllers/Order.js
@@ -1,8 +1,8 @@
 const Order = require("../models/Order");
 
 function create_order(req,res){
-    var products=req.body.products;
-    var username = req.body.username;
+    var products=req.body.fname;
+    var username = req.body.email;
     if(products == null || username == null){
         res.status(400).send("You did not enter all the needed parameters :( Please try again")
     }else {
@@ -14,44 +14,48 @@ function create_order(req,res){
     }
 }
 
-function get_orders(req, res){
-    Order.find().then(results => {
-        res.json(results)
-    });
+function orderForm(req,res){
+    res.render("order",{})
 }
+// function get_orders(req, res){
+//     Order.find().then(results => {
+//         res.json(results)
+//     });
+// }
 
-function deletebyusername (req, res){
-    Order.findOne({
-        "username": req.params.username
-    })
-    .then(results => {
-        Order.findByIdAndDelete(results.id).then(() => res.send('success')).catch(() => res.send('Failure'));
-    });
-}
-function getByname (req, res){
-    Order.find({
-        "username": req.params.username
-    })
-    .then(results => {
-        res.json(results)
-    });
-}
+// function deletebyusername (req, res){
+//     Order.findOne({
+//         "username": req.params.username
+//     })
+//     .then(results => {
+//         Order.findByIdAndDelete(results.id).then(() => res.send('success')).catch(() => res.send('Failure'));
+//     });
+// }
+// function getByname (req, res){
+//     Order.find({
+//         "username": req.params.username
+//     })
+//     .then(results => {
+//         res.json(results)
+//     });
+// }
 
 
-function updatebyid(req, res){
-    Product.findByIdAndUpdate(req.params.id,{"username":req.body.username,"products":req.body.products},
-        function(err,r){
-            if(err){
-                res.status(400).send(err);
-            } else {
-                res.status(200).send(r);
-            }
-    });
-}
+// function updatebyid(req, res){
+//     Product.findByIdAndUpdate(req.params.id,{"username":req.body.username,"products":req.body.products},
+//         function(err,r){
+//             if(err){
+//                 res.status(400).send(err);
+//             } else {
+//                 res.status(200).send(r);
+//             }
+//     });
+// }
 module.exports={
     create_order,
-    get_orders,
-    getByname,
-    deletebyusername,
-    updatebyid
+    orderForm
+    // get_orders,
+    // getByname,
+    // deletebyusername,
+    // updatebyid
 }
