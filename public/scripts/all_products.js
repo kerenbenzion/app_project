0f07813cb3fd1f446e46cd27a3be36fde21e4308
keren ben zion <keren.benzion@perception-point.io>
2022-10-28 20:04:27 +0300

    edit product and icon to the tab

--- public/scripts/all_products.js
+++ public/scripts/all_products.js
@@ -9,3 +9,49 @@ function deleteItem(d){
     }
   });
 }
+
+function openForm(d) {
+  let all_popups = document.getElementsByClassName('form-popup')
+  for(var i = 0;i<all_popups.length;i++){
+    all_popups[i].style.display="none"
+  }
+ 
+  // d.parentNode.parentNode.childNodes.forEach(function(item){
+  //   console.log(item.childNodes.className)
+  // });
+  d.parentElement.getElementsByClassName("form-popup")[0].style.display = "block"
+
+}
+function edititem(d){
+  var id =d.getAttribute("product_id")
+  var index =d.getAttribute("index")
+  console.log(index)
+  var urlCall="/products/updateproduct/"+id
+
+  var description=document.getElementsByName('description')[index].value
+  var size=document.getElementsByName('size')[index].value
+  var name=document.getElementsByName('name')[index].value
+  var type=document.getElementsByName('type')[index].value
+  var price=document.getElementsByName('price')[index].value
+  var data = {
+    "name": name,
+    "description":description,
+    "size":size,
+    "type":type,
+    "price":price
+  }
+  $.ajax({
+    url: urlCall,
+    type: 'PUT',
+    data:data,
+    success: function (result) {
+      window.location.href = "/products";
+    }
+  });
+}
+function closeForm(d) {
+  let all_popups = document.getElementsByClassName('form-popup')
+  for(var i = 0;i<all_popups.length;i++){
+    all_popups[i].style.display="none"
+  }
+}
