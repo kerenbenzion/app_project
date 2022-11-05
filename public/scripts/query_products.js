b5ad0f59f394a8832f78da0edd59d03ee3ac29cc
keren ben zion <keren.benzion@perception-point.io>
2022-11-05 13:57:24 +0200

    isadmin

--- public/scripts/query_products.js
+++ public/scripts/query_products.js
@@ -1,3 +1,3 @@
-function presentProductByCategory(category, color){
-    window.location.href = "/product-category?category="+category+"&color="+color;
+function presentProductByCategory(category, color) {
+    window.location.href = "/product-category?category=" + category + "&color=" + color;
 }
