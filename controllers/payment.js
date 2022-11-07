3f46520e609dd9042cc92e5c21cc967f5b99a2df
keren ben zion <keren.benzion@perception-point.io>
2022-11-07 14:56:45 +0200

    fix payment page

--- controllers/payment.js
+++ controllers/payment.js
@@ -1,11 +1,13 @@
 const { resetWatchers } = require("nodemon/lib/monitor/watch");
 function getpaymentpage(req, res) {
-    res.render('../views/payment');
+    res.render('../views/payment', {
+        username: req.session.username
+    });
 }
 function finishPayment(req, res) {
     res.render('../views/finishPayment');
 }
 
 module.exports = {
-    getpaymentpage,finishPayment
+    getpaymentpage, finishPayment
 }
