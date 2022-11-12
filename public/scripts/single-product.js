4364febdd1b8bc56eca6572f54544dbeb15cf15d
barchen <barchen7200@gmail.com>
2022-11-12 11:26:29 +0200

    add validate to choosing product

--- public/scripts/single-product.js
+++ public/scripts/single-product.js
@@ -44,14 +44,31 @@ $('.nav-item a').click(function (e) {
   $(this).tab('show');
 })
 
+function Validate_options() {
+  var size_option = document.getElementById("color_option");
+  var color_option = document.getElementById("size_option");
+  console.log(size_option.value)
+  if (size_option.value == "בחירת אפשרות" || color_option.value == "בחירת אפשרות") {
+      return false;
+  }
+  return true;
+}
 
 function AddToBasket() {
-  console.log(currnet_product)
-  var id = currnet_product['_id']
-  var preview_img = currnet_product['preview_img']
-  var price = currnet_product['price']
-  var name = currnet_product['name']
-  SaveDataToLocalStorage(id, price, preview_img, name)
+  var is_valid = Validate_options()
+  if (!is_valid){
+    alert("בחר צבע ו/או מידה");
+
+  }
+  else{
+    console.log(currnet_product)
+    var id = currnet_product['_id']
+    var preview_img = currnet_product['preview_img']
+    var price = currnet_product['price']
+    var name = currnet_product['name']
+    SaveDataToLocalStorage(id, price, preview_img, name)
+  }
+
 }
 
 function SaveDataToLocalStorage(id, price, preview_img, name) {
