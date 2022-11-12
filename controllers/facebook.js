c6640a52da505e5738691b488904ef938fad0017
barchen <barchen7200@gmail.com>
2022-11-12 12:53:34 +0200

    add facebook link

--- controllers/facebook.js
+++ controllers/facebook.js
@@ -5,11 +5,9 @@ const { use } = require("../routes/facebook");
 function postfacebook(req,res){
     const {msg} = req.body
     const result = facebookService.publishPost(msg)
-    console.log(result)
     if(result){
-        res.redirect('/products')
+        console.log(result)
     }
-    // res.redirect('/products')
 
 }
 function facebookForm(req,res){
