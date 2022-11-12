2b8576f2d95621daaa1927173fabf6764e4ab647
keren ben zion <keren.benzion@perception-point.io>
2022-11-12 14:09:04 +0200

    orders page

--- routes/Order.js
+++ routes/Order.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const router = express.Router();
-const order  = require("../controllers/Order");
+const order = require("../controllers/Order");
 
-router.post('/order/create_order',order.create_order);
-router.delete('/order/deletebyusername/:name',order.deletebyusername);
-router.put('/order/updatebyid/:id',order.updatebyid);
-router.get('/order/getByname/:username',order.getByname);
+router.post('/order/create_order', order.create_order);
+router.delete('/order/deletebyusername/:name', order.deletebyusername);
+router.put('/order/updatebyid/:id', order.updatebyid);
+router.get('/order', order.getorderspage);
+router.get('/order/getorders', order.get_orders);
+
+router.delete('/order/:id', order.deleteorder);
 
 
 module.exports = router;
