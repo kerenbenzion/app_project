2c52e81507f2a165cde947e1051c754461dde4d3
barchen <barchen7200@gmail.com>
2022-11-20 15:52:41 +0200

    update token

--- services/facebook_server.js
+++ services/facebook_server.js
@@ -3,7 +3,7 @@ const alert = require('alert')
 
 function publishPost(msg){
     const pageId='107964865443633'
-    const access_token = 'EABSw5NTZAolgBAPklXxEy0jqdHuotDdALJzXU4Du4kRdYCEpoSAKZByyjsM33lrqs8kJkRb8Fqv9600QEpfr02F9OZBGL7VJ33KKdd2a0mmebkadZCEBXgEO7xG9ddQvfpJv5dDO3AAsRfS3Mp9GT3F5fY8rDASUEWulkOEesGecbOwbhUzi9mZAuLCuNpPdJsQdW0whcBZAc6RbZCVSVB8'
+    const access_token = 'EABSw5NTZAolgBAGZCnsvKlh9vHqNnBZAIZBq9ZCbixcYIA7ZC9eenZAWnZAC03lFM5eRcBL9Y0gf8YRhPlLZBe5DvFGfC2jjGKZAMHgordw6f1SsKs7B263PbxydkSLnn8OMl1Fa5t17hjjZB81rZAB2P4oQz29CNSMC7kvbuVMAOrX2XFvFeyRdMSLdmJQuRdQps6xq08ZAaogFdTQZDZD'
     axios.post(`https://graph.facebook.com/${pageId}/feed?message=${msg}&access_token=${access_token}`, null)
     .then(function (response){
         console.log(response)
